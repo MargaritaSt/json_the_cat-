98d8a947bb6103a4169d91c2ba92c6e031193894
MargaritaSt <margarita.struts@me.com>
2020-06-25 02:38:59 +0000

    add test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
-const argument = process.argv;
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + argument[2];
 const request = require('request');
- 
-request(url,(error,response,body) => {
-  if (error) {
-    console.log(error);
-    return error;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Cat has not been found!");
-  } else {
-    console.log(data[0].description);
-    console.log(typeof data);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url,(error,response,body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(error, "Cat has not been found!");
+    } else {
+      callback(error,data[0].description);
+      console.log(typeof data[0].description);
+      //console.log(data[0].description);
+    }
+  });
+};
+module.exports = fetchBreedDescription;
